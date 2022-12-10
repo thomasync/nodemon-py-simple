80064912aec54d82537c33bbad630c6a1ef83748
Thomas <github@thomascauquil.fr>
2022-12-10 06:29:05 +0100

    add(0.0.1) download_url

--- setup.py
+++ setup.py
@@ -6,6 +6,7 @@ setup(
     version="0.0.1",
     license='MIT',
     description="Nodemon like for python, very simple and without dependencies",
+    download_url="https://github.com/thomasync/nodemon-py-simple/archive/refs/tags/0.0.1.tar.gz",
     scripts=["nodemon-py-simple/main.py"],
     author="thomasync",
     url="https://github.com/thomasync/nodemon-py-simple.git",
